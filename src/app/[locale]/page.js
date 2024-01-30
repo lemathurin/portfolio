2459f2a43d0cfb12b4246fe1171c49630ffa7466
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-01-30 14:15:36 +0100

    Correct font wouldn't load. Beginning of a navbar

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,10 +1,21 @@
 import { useTranslations } from "next-intl";
+import NavBar from "./components/Navbar";
 
 export default function Home() {
-  const t = useTranslations("Index");
+  const t = useTranslations("Home");
   return (
-    <div>
-      <h1>{t("title")}</h1>
+    <div className="w-screen bg-lightMode-background flex-col justify-start items-center py-20 gap-40 inline-flex">
+      <NavBar />
+      <h1 className="text-lightMode-text text-9xl">{t("greeting")}</h1>
+      <div className="max-w-[700px] flex-col justify-start items-start inline-flex">
+        <p className="text-justify text-lightMode-text text-5xl">{t("p-1")}</p>
+      </div>
+      <div className="max-w-[700px] flex-col justify-start items-start inline-flex">
+        <p className="text-justify text-lightMode-text text-5xl">{t("p-2")}</p>
+      </div>
+      <div className="max-w-[700px] flex-col justify-start items-start inline-flex">
+        <p className="text-justify text-lightMode-text text-5xl">{t("p-3")}</p>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        lightMode: {
+          background: "#FDFDFC",
+          text: "#1D211C",
+        },
+        darkMode: {
+          background: "#18181A",
+          text: "#EDEEF0",
+        },
+      },
+      fontFamily: {
+        EditorialNew: ["var(--font-EditorialNew)"],
       },
     },
   },
