fb303313736d5219e613ed2d70d0c87bc29c59de
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-02-19 14:06:34 +0100

    Dropdown appears due to useState

--- src/app/[locale]/components/Navbar.js
+++ src/app/[locale]/components/Navbar.js
@@ -1,79 +1,83 @@
+"use client";
+
 import Link from "next/link";
 import "../components/Navbar.css";
 import Image from "next/image";
-
-// for the dropdown
-// https://www.youtube.com/watch?v=VQWu4e6agPc
+import { useState } from "react";
 
 export default function NavBar(props) {
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+
   return (
     <div className="entire-navbar">
-      <div className="project-dropdown">
-        <div className="project-left dropdown-boxes">
-          <h3 className="dropdown-text">{props.presenterApp}</h3>
-          <Image
-            className="shadow"
-            src="/images/presenter_app.png"
-            width={400}
-            height={210}
-            alt="Screenshot of the unfinished project"
-          />
-        </div>
-        <div className="project-right-section">
-          <div className="project-right-small dropdown-boxes">
-            <h3 className="dropdown-text">{props.pico8}</h3>
-            <div className="project-right-small-images">
-              <Image
-                className="shadow"
-                src="/images/pico-8_loading.png"
-                width={83}
-                height={83}
-                alt="Screenshot of game loading screen"
-              />
-              <Image
-                className="shadow"
-                src="/images/pico-8_menu.png"
-                width={83}
-                height={83}
-                alt="Screenshot of game menu screen"
-              />
-              <Image
-                className="shadow"
-                src="/images/pico-8_game.png"
-                width={83}
-                height={83}
-                alt="Screenshot of game"
-              />
+      {isButtonHovered && (
+        <div className="project-dropdown">
+          <div className="project-left dropdown-boxes">
+            <h3 className="dropdown-text">{props.presenterApp}</h3>
+            <Image
+              className="shadow"
+              src="/images/presenter_app.png"
+              width={400}
+              height={210}
+              alt="Screenshot of the unfinished project"
+            />
+          </div>
+          <div className="project-right-section">
+            <div className="project-right-small dropdown-boxes">
+              <h3 className="dropdown-text">{props.pico8}</h3>
+              <div className="project-right-small-images">
+                <Image
+                  className="shadow"
+                  src="/images/pico-8_loading.png"
+                  width={83}
+                  height={83}
+                  alt="Screenshot of game loading screen"
+                />
+                <Image
+                  className="shadow"
+                  src="/images/pico-8_menu.png"
+                  width={83}
+                  height={83}
+                  alt="Screenshot of game menu screen"
+                />
+                <Image
+                  className="shadow"
+                  src="/images/pico-8_game.png"
+                  width={83}
+                  height={83}
+                  alt="Screenshot of game"
+                />
+              </div>
+            </div>
+            <div className="project-right-small dropdown-boxes">
+              <h3 className="dropdown-text">{props.moreProjects}</h3>
+              <div className="project-right-small-images">
+                <Image
+                  className="shadow"
+                  src="/images/eym.png"
+                  width={83}
+                  height={210}
+                  alt="Screenshot of game loading screen"
+                />
+                <Image
+                  className="shadow"
+                  src="/images/portfolio_exploration.png"
+                  width={83}
+                  height={210}
+                  alt="Screenshot of game loading screen"
+                />
+                <Image
+                  className="shadow"
+                  src="/images/dataviz.png"
+                  width={110}
+                  height={83}
+                  alt="Screenshot of game loading screen"
+                />
+              </div>
             </div>
           </div>
-          <div className="project-right-small dropdown-boxes">
-            <h3 className="dropdown-text">{props.moreProjects}</h3>
-            <div className="project-right-small-images">
-              <Image
-                className="shadow"
-                src="/images/eym.png"
-                width={83}
-                height={210}
-                alt="Screenshot of game loading screen"
-              />
-              <Image
-                className="shadow"
-                src="/images/portfolio_exploration.png"
-                width={83}
-                height={210}
-                alt="Screenshot of game loading screen"
-              />
-              <Image
-                className="shadow"
-                src="/images/dataviz.png"
-                width={110}
-                height={83}
-                alt="Screenshot of game loading screen"
-              />
-            </div>
-          </div>
         </div>
-      </div>
+      )}
       <ol className="menu-button-container">
         <Link href="/">
           <li className="menu-button cursor-cell">
@@ -83,7 +87,11 @@ export default function NavBar(props) {
           </li>
         </Link>
         <Link href="/">
-          <li className="menu-button">
+          <li
+            className="menu-button"
+            onMouseEnter={() => setIsButtonHovered(true)}
+            onMouseLeave={() => setIsButtonHovered(false)}
+          >
             <span className="menu-text" href="/">
               {props.projects}
             </span>
