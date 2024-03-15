5ac10164bae9c48557914c9a3673d66080ad0f6d
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-15 15:33:43 +0100

    i18n fix?

--- src/app/[locale]/contact/page.js
+++ src/app/[locale]/contact/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useTranslations } from "next-intl";
 
 export default function contact() {
-  const n = useTranslations("contact");
+  const c = useTranslations("contact");
   return (
     <div className="w-screen h-screen flex items-start md:items-center justify-center bg-lightMode-background">
       <div className="flex flex-col p-10 pb-20 md:grid md:grid-cols-3 md:gap-5">
@@ -10,43 +10,43 @@ export default function contact() {
           <Link href="/">Email</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("email")}
+          {c("email")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:text-lightMode-hoveredtext cursor-not-allowed">
           LinkedIn
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("linkedin")}
+          {c("linkedin")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://github.com/lemathurin">GitHub</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("github")}
+          {c("github")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://twitter.com/mathurin_sekine">Twitter</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("twitter")}
+          {c("twitter")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://dribbble.com/mathurin">Dribbble</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("dribbble")}
+          {c("dribbble")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://layers.to/mathurin">Layers</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("layers")}
+          {c("layers")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://read.cv/mathurin">read.cv</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          {n("readcv")}
+          {c("readcv")}
         </div>
       </div>
     </div>
