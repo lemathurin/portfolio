d56b1cfb1ad7a2ef363bb07f7deb3a3c050c618b
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-15 15:28:25 +0100

    added translations to contact page

--- src/app/[locale]/contact/page.js
+++ src/app/[locale]/contact/page.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export default function contact() {
+  const n = useTranslations("contact");
   return (
     <div className="w-screen h-screen flex items-start md:items-center justify-center bg-lightMode-background">
       <div className="flex flex-col p-10 pb-20 md:grid md:grid-cols-3 md:gap-5">
@@ -8,43 +10,43 @@ export default function contact() {
           <Link href="/">Email</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          I’ll answer as soon as possible
+          {n("email")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:text-lightMode-hoveredtext cursor-not-allowed">
           LinkedIn
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          coming soon
+          {n("linkedin")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://github.com/lemathurin">GitHub</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          see more of my dev work
+          {n("github")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://twitter.com/mathurin_sekine">Twitter</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          I don’t post often
+          {n("twitter")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://dribbble.com/mathurin">Dribbble</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          some of my design work is here
+          {n("dribbble")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://layers.to/mathurin">Layers</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          and also here
+          {n("layers")}
         </div>
         <div className="font-instrument md:text-right text-3xl hover:underline hover:-rotate-3 md:hover:rotate-3 origin-bottom-left md:origin-bottom-right hover:text-lightMode-hoveredtext transition">
           <Link href="https://read.cv/mathurin">read.cv</Link>
         </div>
         <div className="font-inter font-extralight italic text-xl flex items-end col-span-2 pb-8 md:pb-0">
-          my cv is available here
+          {n("readcv")}
         </div>
       </div>
     </div>
