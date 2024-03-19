6634f48ae2b39f8cf4802c6ddcffa62507747f7c
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-19 12:13:28 +0100

    added i18n

--- src/app/[locale]/components/ProjectsList.jsx
+++ src/app/[locale]/components/ProjectsList.jsx
@@ -1,61 +1,63 @@
-export default function ProjectsList() {
+"use client";
+
+export default function ProjectsList(props) {
   return (
     <div className="w-screen flex md:items-start md:justify-center">
       <div className="flex flex-col items-start gap-5 p-10 pb-36 w-full max-w-[700px] *:w-full *:flex *:flex-col *:md:flex *:md:flex-row *:md:justify-between *:md:items-center *:md:content-center">
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            Portfolio
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.portfolio}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            personal project
+            {props.personalproject}
           </p>
         </div>
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            e-commerce store
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.ecommerce}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            school project
+            {props.schoolproject}
           </p>
         </div>
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            Dataviz website
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.dataviz}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            school project
+            {props.schoolproject}
           </p>
         </div>
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            Chrome extension
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.chromeextension}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            school project
+            {props.schoolproject}
           </p>
         </div>
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            Pico-8 game
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.pico8game}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            school project
+            {props.schoolproject}
           </p>
         </div>
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            Presenter app
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.presenterapp}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            personal project
+            {props.personalproject}
           </p>
         </div>
         <div>
-          <h2 className="font-instrument text-3xl text-lightMode-text">
-            PHP social network
+          <h2 className="font-instrument text-3xl text-lightMode-text hover:underline hover:-rotate-3 origin-bottom-left hover:text-lightMode-hoveredtext transition">
+            {props.phpsocialnetwork}
           </h2>
           <p className="font-inter text-xl text-lightMode-text font-extralight">
-            school project
+            {props.schoolproject}
           </p>
         </div>
       </div>
--- src/app/[locale]/projects/page.js
+++ src/app/[locale]/projects/page.js
@@ -1,9 +1,21 @@
+import { useTranslations } from "next-intl";
 import ProjectsList from "../components/ProjectsList";
 
 export default function Projects() {
+  const p = useTranslations("Projects");
   return (
-    <div className="">
-      <ProjectsList />
+    <div>
+      <ProjectsList
+        portfolio={p("portfolio")}
+        ecommerce={p("ecommerce")}
+        dataviz={p("dataviz")}
+        chromeextension={p("chromeextension")}
+        pico8game={p("pico8game")}
+        presenterapp={p("presenterapp")}
+        phpsocialnetwork={p("phpsocialnetwork")}
+        schoolproject={p("schoolproject")}
+        personalproject={p("personalproject")}
+      />
     </div>
   );
 }
