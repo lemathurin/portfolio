9d07a25091170ed005e5c3b6cd1009823a129631
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-24 17:46:34 +0100

    adding components

--- src/app/[locale]/projects/ecommerce/page.js
+++ src/app/[locale]/projects/ecommerce/page.js
@@ -1,80 +1,48 @@
+import {
+  CaseStudyVideo,
+  CaseStudyTitle,
+  CaseStudyDetails,
+  CaseStudyParagraph,
+  CaseStudyH2,
+  CaseStudyList,
+} from "../../components/CaseStudy";
+
 export default function Ecommerce() {
+  const techStack = [
+    { name: "Figma", link: "https://www.figma.com/" },
+    { name: "React Vite", link: "https://vitejs.dev/" },
+    { name: "Shadcn/ui", link: "https://ui.shadcn.com/" },
+    { name: "Tailwindcss", link: "https://tailwindcss.com/" },
+    { name: "Supabase", link: "https://supabase.com/" },
+    { name: "Express.js", link: "https://expressjs.com/" },
+  ];
+  const teammates = [
+    { name: "Tianhong Huang", link: "https://github.com/Tianhong258" },
+    { name: "Fabienne Lis", link: "https://github.com/FabienneLIS" },
+    { name: "Alexis Moulliere", link: "https://github.com/realalexis" },
+    { name: "Marie Le Lamer", link: "https://github.com/MarieLeLamer" },
+    { name: "Audrey Gueguen", link: "https://github.com/AudreyGgn" },
+    { name: "Maguelone Guinaud", link: "https://github.com/magueloneguinaud" },
+    { name: "Yannick Oudin", link: "https://github.com/oudinyannick" },
+    { name: "Anna Tehiva", link: "https://github.com/annatehiva" },
+    { name: "Florentin Corbiat", link: "https://github.com/CorbiatFlorentin" },
+  ];
   return (
     <main className="w-screen flex items-start justify-center">
-      <div className="flex flex-col p-10 pb-36 w-full max-w-[700px]">
-        <video autoPlay loop muted>
-          <source
-            width={"100%"}
-            src="/videos/smallAdopteUnMeuble.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-        <h1 className="font-instrument text-lightMode-text text-5xl mt-10">
-          E-commerce store project
-        </h1>
-        <h3 className="font-inter text-lightMode-text text-xl font-semibold">
-          school project
-        </h3>
-        <h3 className="font-inter text-lightMode-text text-xl font-semibold">
-          04/03/2024 -&gt; 14/03/2024
-        </h3>
-        <a
-          href="https://github.com/adatechschool/projet-collectif-plateforme-de-meubles-adopte-un-meuble"
-          className="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-xl font-semibold"
-        >
-          View Github repository
-        </a>
-        <p className="font-inter text-lightMode-text text-lg">
-          The goal of this project was to have us organize ourselves in front
-          and back-end teams and ensure the delivery of the website. Aute
-          laborum et non do aliqua. Ea sunt culpa exercitation magna. Excepteur
-          eu est Lorem ut aliqua quis deserunt. Veniam velit minim eu tempor eu
-          ipsum.
-        </p>
-        <h2 className="font-instrument text-lightMode-text text-4xl">
-          Tech stack and tools
-        </h2>
-        <ul className="font-inter text-lightMode-text text-lg">
-          <li>Figma</li>
-          <li>React Vite</li>
-          <li>Shadcn/ui</li>
-          <li>Tailwindcss</li>
-          <li>Supabase</li>
-          <li>Express.js</li>
-        </ul>
-        <h2 className="font-instrument text-lightMode-text text-4xl">
-          Teammates
-        </h2>
-        <ul>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-figma">Audrey Gueguen</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-react-vite">Tianhong Huang</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-shadcn-ui">Fabienne Lis</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-tailwindcss">Marie Lelamer</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-supabase">Alexis Moulliere</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-express-js">Anna Tehiva</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-figma">Yannick Oudin</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-figma">Florentin Corbiat</a>
-          </li>
-          <li class="font-inter text-lightMode-hoveredtext hover:text-lightMode-text hover:underline text-lg">
-            <a href="link-to-figma">Maguelone Guinaud</a>
-          </li>
-        </ul>
+      <div className="flex flex-col md:p-10 pb-36 w-[90%] md:w-full max-w-[700px]">
+        <CaseStudyVideo videoPath="/videos/smallAdopteUnMeuble.mp4" />
+        <CaseStudyTitle title="E-commerce store project" />
+        <CaseStudyDetails
+          projectType="school project"
+          duration="04/03/2024 -&gt; 14/03/2024"
+          linkName="View Github repository"
+          link="https://github.com/adatechschool/projet-collectif-plateforme-de-meubles-adopte-un-meuble"
+        />
+        <CaseStudyParagraph text="The goal of this project was to have us organize ourselves in front and back-end teams and ensure the delivery of the website. Aute laborum et non do aliqua. Ea sunt culpa exercitation magna. Excepteur eu est Lorem ut aliqua quis deserunt. Veniam velit minim eu tempor eu ipsum." />
+        <CaseStudyH2 title="Tech stack and tools used" />
+        <CaseStudyList items={techStack} />
+        <CaseStudyH2 title="Teammates" />
+        <CaseStudyList items={teammates} />
       </div>
     </main>
   );
