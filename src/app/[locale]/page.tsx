316e0f0930307f867374d90a49d5d2d835e34b65
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-07-15 23:32:30 +0200

    Added links

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,29 +1,88 @@
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import { Link } from "@/i18n/navigation";
+import Clock from "@/components/Clock";
+import LanguageToggle from "@/components/LanguageToggle";
+import Link from "next/link";
 
 export default function Home() {
   const t = useTranslations();
 
   return (
     <main className="flex justify-center">
-      <section className="max-w-96 text-justify text-lg flex flex-col gap-4">
-        <div className="w-full h-36 bg-[#FFCD34] relative">
+      <article className="max-w-96 text-justify flex flex-col gap-9 py-9">
+        <header className="flex justify-between items-center w-full gap-4">
+          <LanguageToggle />
+          <Clock />
+        </header>
+
+        <figure className="w-full h-36 bg-[var(--yellow)] relative overflow-hidden flex items-end justify-center">
           <Image
             src="/me-100.svg"
-            alt="Mathurin Sekine SVG"
-            fill
-            className="object-contain object-bottom"
+            alt="Mathurin Sekine's avatar"
+            role="img"
+            height={150}
+            width={150}
+            className="object-contain"
           />
-        </div>
-        <p>{t("intro.greeting")}</p>
-        <p>{t("intro.experience")}</p>
-        <p>{t("intro.project")}</p>
-        <p>{t("intro.skills")}</p>
-        <p>{t("intro.contact")}</p>
-        <p>{t("intro.closing")}</p>
-        <p>{t("intro.signature")}</p>
-      </section>
+        </figure>
+
+        <section aria-labelledby="intro-heading">
+          <h1 id="intro-heading" className="sr-only">
+            About me
+          </h1>
+          <p>{t("intro.wave")}</p>
+          <br />
+          <p>{t("intro.greeting")}</p>
+          <br />
+          <p>
+            {t.rich("intro.experience", {
+              juisci: (chunks) => (
+                <Link target="_blank" href="https://juisci.com">
+                  {chunks}
+                </Link>
+              ),
+              ada: (chunks) => (
+                <Link target="_blank" href="https://adatechschool.fr/">
+                  {chunks}
+                </Link>
+              ),
+            })}
+          </p>
+          <br />
+          <p>{t("intro.project")}</p>
+          <br />
+          <p>{t("intro.skills")}</p>
+          <br />
+          <p>
+            {t.rich("intro.contact", {
+              linkedin: (chunks) => (
+                <Link
+                  target="_blank"
+                  href="https://www.linkedin.com/in/mathurinsekine/"
+                >
+                  {chunks}
+                </Link>
+              ),
+              twitter: (chunks) => (
+                <Link target="_blank" href="https://x.com/mathurin_sekine">
+                  {chunks}
+                </Link>
+              ),
+              mailto: (chunks) => (
+                <Link target="_blank" href="mailto:mathurinsekine@gmail.com">
+                  {chunks}
+                </Link>
+              ),
+              email: (chunks) => (
+                <span className="font-mono opacity-70 text-sm">{chunks}</span>
+              ),
+            })}
+          </p>
+          <br />
+          <p>{t("intro.closing")}</p>
+          <p>{t("intro.signature")}</p>
+        </section>
+      </article>
     </main>
   );
 }
