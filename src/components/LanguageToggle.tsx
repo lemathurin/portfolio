b5d3242dea1cd0f2cd36ef3d29100ff720f68651
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-07-16 16:22:59 +0200

    Fixed languageToggle component

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import { useTranslations, useLocale } from "next-intl";
-import { useRouter } from "@/i18n/navigation";
+import { useRouter, usePathname } from "@/i18n/navigation";
 
 export default function LanguageToggle() {
   const t = useTranslations();
   const router = useRouter();
   const locale = useLocale();
+  const pathname = usePathname();
 
   function toggleLanguage() {
     const newLocale = locale === "en" ? "fr" : "en";
-    router.push("/", { locale: newLocale });
+    router.push(pathname, { locale: newLocale });
   }
 
   return (
     <button
       onClick={toggleLanguage}
-      className="font-mono text-sm underline hover:no-underline opacity-70 hover:opacity-100 cursor-pointer"
+      className="cursor-pointer font-mono text-sm underline opacity-70 hover:no-underline hover:opacity-100"
       aria-label={t("languageToggle")}
     >
       {t("languageToggle")}
