729a84abb9f08729f9916fdfaa61cf3976dc68a2
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-07-17 13:24:32 +0200

    Added redirection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/projects",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/projects/portfolio",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/projects/ecommerce",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
