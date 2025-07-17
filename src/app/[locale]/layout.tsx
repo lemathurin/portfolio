656a35b4748983cad0f82a8ed1188b1166192879
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-07-17 13:32:46 +0200

    og image

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,8 +15,28 @@ export async function generateMetadata({
   const t = await getTranslations({ locale, namespace: "Metadata" });
 
   return {
+    metadataBase: new URL("https://mathurinsekine.fr"),
     title: t("title"),
     description: t("description"),
+    openGraph: {
+      title: t("title"),
+      description: t("description"),
+      images: [
+        {
+          url: "/og-image.png",
+          width: 1200,
+          height: 630,
+          alt: t("title"),
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: t("title"),
+      description: t("description"),
+      images: ["/og-image.png"],
+    },
   };
 }
 
