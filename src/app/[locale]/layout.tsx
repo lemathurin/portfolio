10c603b80e4a23fd808ee5834852dd58c5d0b6cb
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-07-23 15:34:14 +0200

    Optimized og image

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,7 +29,7 @@ export async function generateMetadata({
       description: t("description"),
       images: [
         {
-          url: "/og-image.png",
+          url: "/og-image.webp",
           width: 1200,
           height: 630,
           alt: t("title"),
@@ -41,7 +41,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title: t("title"),
       description: t("description"),
-      images: ["/og-image.png"],
+      images: ["/og-image.webp"],
     },
   };
 }
