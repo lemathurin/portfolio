05f13a45a9d42b5c89d412523600375ffa8e980c
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-07-28 23:12:29 +0200

    Handle not-found Header link

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -5,7 +5,7 @@ import MDXContent from "@/components/MDXContent";
 export default function NotFound() {
   return (
     <main className="mx-auto flex w-full max-w-lg flex-col justify-center gap-9 p-5 md:p-0 md:py-5">
-      <Header />
+      <Header isNotFoundPage={true} />
       <article className="prose">
         <MDXContent src="404.mdx" />
       </article>
